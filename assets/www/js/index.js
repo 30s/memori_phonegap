fc9eb61c29b28056e0a0325984cdaf55d2762465
Robert <rzheng@xtremeprog.com>
2012-10-11 19:18:06 +0800

    * weibo login with childBrowser; * memori site login with localStorage; * CORS problem

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -47,3 +47,87 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+$.support.cors = true;
+
+var cb = window.plugins.childBrowser;
+var ls = window.localStorage;
+
+var client_id = "500e29a921085c3e19000000";
+var url_api = "http://192.168.1.138:8000/";
+
+var api_register_by_weibo = "v1/account/register_by_weibo/";
+var api_shared_item_by_me = "v1/get/share/";
+
+var memori_register_by_weibo = function () {
+	$.post(url_api + api_register_by_weibo, 
+			{
+				"uid": ls.getItem("uid"),
+				"screen_name": ls.getItem("screen_name"),
+				"avatar": ls.getItem("avatar"),
+				"token": ls.getItem("access_token"),
+				"expires_in": ls.getItem("expires_in"),
+				"client_id": client_id
+			},
+			function (info) {
+				if ( info["error_code"] ) {
+					alert(info["error_message"]);
+				} else {
+					ls.setItem("m_token", info["token"]);
+					ls.setItem("m_uid", info["uid"]);
+					ls.setItem("email_name", info["email_name"]);
+					show_profile();
+				}
+			});	
+};
+
+var show_profile = function() {
+	$("#login").hide();
+	$("#email_name").text(ls.getItem("screen_name"));
+	$("#avatar")[0].src = ls.getItem("avatar");
+	$("#uid").text(ls.getItem("m_uid"));
+	$("#profile").show();
+};
+
+$(document).bind('pageinit', function() {
+	if ( ls.getItem("m_uid") != undefined ) {
+		show_profile();
+	}
+	
+	$("#btn_weibo_login").click(function() {
+		cb.showWebPage("https://api.weibo.com/oauth2/authorize?client_id=866081245&display=mobile&response_type=token&redirect_uri=http://www.memori.cn/memori/connect/sina/callback", { showLocationBar: false });		
+		cb.onLocationChange = function (loc){
+			if ( loc.indexOf("access_token") != -1 ) { 
+				ls.setItem("access_token", loc.substring(loc.indexOf("#access_token") + 14, loc.indexOf("&remind_in")));
+				ls.setItem("expires_in", loc.substring(loc.indexOf("&remind_in") + 11, loc.indexOf("&expires_in")));
+				ls.setItem("uid", loc.substring(loc.indexOf("&uid") + 5));
+
+				$.get("https://api.weibo.com/2/users/show.json", 
+						{"access_token": ls.getItem("access_token"), "uid": ls.getItem("uid")},
+						function (info) {
+							ls.setItem("screen_name", info["screen_name"]);
+							ls.setItem("avatar", info["profile_image_url"]);
+						});
+				memori_register_by_weibo();				
+				cb.close();
+			}
+		};
+	});
+	
+	$("#btn_logout").click(function () {
+		$("#profile").hide();
+		$("#login").show();
+	});
+	
+	$("#btn_records").click(function () {
+		$.ajax({
+			url: url_api + api_shared_item_by_me,
+			type: "GET",
+			dataType: "jsonp",
+			headers: {"HTTP_AUTHORIZATION": "Bearer " + ls.getItem("token")},
+			success: function (data) {
+				alert(data);
+			}
+			});
+	});
+});
